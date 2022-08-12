859c271f02ec811585f222ca384a62ac21b454d1
Furqon-M <furchon.muddafar@gmail.com>
2022-08-12 21:36:58 +0700

    jwt auth, generate token

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"mengayuh/auth"
 	"mengayuh/handler"
 	"mengayuh/user"
 
@@ -20,8 +21,9 @@ func main() {
 
 	userRepository := user.NewRepository(db)
 	userService := user.NewService(userRepository)
+	authService := auth.NewService()
 
-	userHandler := handler.NewUserHandler(userService)
+	userHandler := handler.NewUserHandler(userService, authService)
 
 	router := gin.Default()
 	api := router.Group("/api/v1")
