0b91c5434ea64ba3b4a6ef07ed379a7a20a4ba72
Furqon-M <furchon.muddafar@gmail.com>
2022-09-10 17:24:43 +0700

    Campaign Repository

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"mengayuh/auth"
+	"mengayuh/campaign"
 	"mengayuh/handler"
 	"mengayuh/helper"
 	"mengayuh/user"
@@ -24,6 +26,21 @@ func main() {
 	}
 
 	userRepository := user.NewRepository(db)
+	campaignRepository := campaign.NewRepository(db)
+
+	campaigns, err := campaignRepository.FindByUserID(1)
+	fmt.Println("debug")
+	fmt.Println("debug")
+	fmt.Println(len(campaigns))
+	for _, campaign := range campaigns {
+		fmt.Println(campaign.Name)
+		if len(campaign.CampaignImages) > 0 {
+			fmt.Println("jumlah gambar")
+			fmt.Println(len(campaign.CampaignImages))
+			fmt.Println(campaign.CampaignImages[0].FileName)
+		}
+	}
+
 	userService := user.NewService(userRepository)
 	authService := auth.NewService()
 
