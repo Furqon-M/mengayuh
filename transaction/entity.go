fef330b24aed28f2c4490b6bce8faceee704ccc7
Furqon-M <furchon.muddafar@gmail.com>
2022-11-12 14:10:53 +0700

    update campaign trassaction

--- transaction/entity.go
+++ transaction/entity.go
@@ -1,14 +1,18 @@
 package transaction
 
-import "time"
+import (
+	"mengayuh/user"
+	"time"
+)
 
 type Transaction struct {
-	ID        int
-	Campaign  int
-	userID    int
-	Amount    int
-	Status    string
-	Code      string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID         int
+	CampaignID int
+	UserID     int
+	Amount     int
+	Status     string
+	Code       string
+	User       user.User
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
 }
